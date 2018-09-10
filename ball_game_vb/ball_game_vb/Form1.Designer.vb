b591251d4d9d07f5cc2de3c424389f8942c5bccf
Heman Kundu <35860379+hemankundu@users.noreply.github.com>
2018-09-10 23:13:38 +0530

    Add files via upload

---
+++ ball_game_vb/ball_game_vb/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.bar = New Microsoft.VisualBasic.PowerPacks.RectangleShape()
+        Me.ball = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.bar, Me.ball})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(557, 493)
+        Me.ShapeContainer1.TabIndex = 0
+        Me.ShapeContainer1.TabStop = False
+        '
+        'bar
+        '
+        Me.bar.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.bar.BorderStyle = System.Drawing.Drawing2D.DashStyle.DashDot
+        Me.bar.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid
+        Me.bar.Location = New System.Drawing.Point(71, 430)
+        Me.bar.Name = "bar"
+        Me.bar.Size = New System.Drawing.Size(153, 5)
+        '
+        'ball
+        '
+        Me.ball.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.ball.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.BackwardDiagonal
+        Me.ball.Location = New System.Drawing.Point(122, 393)
+        Me.ball.Name = "ball"
+        Me.ball.Size = New System.Drawing.Size(36, 36)
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(439, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(232, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(149, 471)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(236, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Move left: A | Move right: D | Launch ball: Space"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(557, 493)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents bar As Microsoft.VisualBasic.PowerPacks.RectangleShape
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ball As Microsoft.VisualBasic.PowerPacks.OvalShape
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ ball_game_vb/ball_game_vb/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form1
+    Dim binfo As BallClass
+    Dim score, life As Integer
+    Public Class BallClass
+        Public rad, c As Double
+        Public dir As Char
+        Public dx, dy As Integer
+        Public Sub calculatec()
+            Dim pt As Point = Form1.ball.Location
+            c = pt.Y - (Math.Tan(rad) * pt.X)
+        End Sub
+        Public Sub move()
+            Dim cur As Point = Form1.ball.Location
+            If Me.rad < Math.PI / 18 Then
+                Me.rad -= Math.PI / 90
+            End If
+            If dir = "x" Then
+                cur.X += dx
+                cur.Y = (Math.Tan(rad) * cur.X) + c
+            ElseIf dir = "y" Then
+                cur.Y += dy
+                cur.X = (cur.Y - c) / Math.Tan(rad)
+            End If
+            Form1.ball.Location = cur
+            Form1.Label3.Text = ((180 / Math.PI) * Me.rad).ToString
+        End Sub
+    End Class
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim cur As Point = ball.Location
+        If cur.X <= 0 Or cur.X + ball.Width >= Me.Width Then
+
+            binfo.dx *= (-1)
+            binfo.dir = "x"
+            binfo.rad = Math.PI - binfo.rad
+            binfo.calculatec()
+
+        ElseIf cur.Y <= 0 Then
+            'binfo.dx *= (-1)
+            'binfo.dir = "y"
+            binfo.rad = (Math.PI - binfo.rad)
+            binfo.calculatec()
+        ElseIf cur.Y >= Me.Height Then
+            life -= 1
+            Label1.Text = "Lives: " + life.ToString
+            initGame()
+            If life = 0 Then
+                MessageBox.Show("Your score: " + score.ToString, "Game over!")
+                resetGame()
+            End If
+
+        ElseIf (cur.Y + ball.Height <= bar.Top + 3) And (cur.Y + ball.Height >= bar.Top) And (ball.Left >= bar.Left) And (ball.Left + (ball.Width / 2) <= bar.Left + bar.Width) Then
+            'Label2.Text = "ball left : " + ball.Left.ToString + "ball w: " + ball.Width.ToString + "bar.le: " + bar.Left.ToString + "bar. wi: " + bar.Width.ToString
+            ball.Top = bar.Top - ball.Height
+            binfo.rad = (Math.PI - binfo.rad)
+            binfo.calculatec()
+            score += 10
+            Label2.Text = "Score: " + score.ToString
+            'Timer1.Enabled = False
+        End If
+        binfo.move()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        resetGame()
+    End Sub
+
+    Private Sub initGame()
+        Timer1.Enabled = False
+        binfo = New BallClass
+        binfo.dx = -5
+        binfo.dy = -5
+        binfo.dir = "x"
+        bar.Left = (Me.Width / 2) - (bar.Width / 2)
+        ball.Location = New Point(bar.Location.X + (bar.Width / 2) - (ball.Width / 2), bar.Location.Y - ball.Height - 3)
+        binfo.rad = (Math.PI / (4 + Rnd(4)))
+        binfo.calculatec()
+    End Sub
+    Private Sub resetGame()
+        initGame()
+        score = 0
+        life = 3
+        Label1.Text = "Lives: " + life.ToString
+        Label2.Text = "Score: " + score.ToString
+    End Sub
+    Private Sub bar_keypress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles bar.KeyPress, MyBase.KeyPress
+        Dim pt As Point = bar.Location
+        If e.KeyChar = "a" Then
+            bar.Left -= 50
+        ElseIf e.KeyChar = "d" Then
+            bar.Left += 50
+        ElseIf e.KeyChar = " " Then
+            If Timer1.Enabled = False Then
+                Timer1.Enabled = True
+            End If
+        End If
+        If bar.Left < 0 Then
+            bar.Left = 0
+        ElseIf bar.Left + bar.Width > Me.Width Then
+            bar.Left = Me.Width - bar.Width
+        End If
+
+    End Sub
+
+End Class
